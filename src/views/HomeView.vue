f030094e5dc229c3316af0874a357bed9e9148b0
Sara Garcia Mateo <sara.garcia.dev@gmail.com>
2026-06-10 15:02:04 +0200

    feat: connected HomeView with destinations store & rendered destinations cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,27 @@
 <template>
+  <div>
+    <div class="destinations-grid">
+      <DestinationCard
+        v-for="destination in filteredDestinations"
+        :key="destination.id"
+        :destination="destination"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useDestinationsStore } from "../stores/destinations";
+import DestinationCard from "../components/DestinationCard.vue";
 
+const destinationsStore = useDestinationsStore();
+const { filteredDestinations } = storeToRefs(destinationsStore);
+
+onMounted(() => {
+  destinationsStore.fetchAllDestinations();
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
